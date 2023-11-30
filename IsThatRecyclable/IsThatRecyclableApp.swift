2db9f6d7fdee3e4b2f0b024ff28c0c71cbc50d7f
SwiftlyNoah <devnoahapps@gmail.com>
2023-11-30 08:28:44 -0500

    Initial Commit

---
+++ IsThatRecyclable/IsThatRecyclableApp.swift
@@ -0,0 +1,17 @@
+//
+//  IsThatRecyclableApp.swift
+//  IsThatRecyclable
+//
+//  Created by Noah Brauner on 11/30/23.
+//
+
+import SwiftUI
+
+@main
+struct IsThatRecyclableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
